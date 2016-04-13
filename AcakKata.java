c18b082a372977f0e18b5a4b91423e0b76824b76
Irvi Firqotul Aini <irvi.fa@gmail.com>
2016-04-13 15:46:15 +0700

    add comment adn run

--- AcakKata.java
+++ AcakKata.java
@@ -14,28 +14,53 @@ public class AcakKata {
 	private static final String MAIN = "MAIN";
 	private ArrayList<String> listKata;
 	BufferedReader in = new BufferedReader(new InputStreamReader (System.in));
-
+	
+	/**
+	 * Inisialisasi
+	 * */
 	public AcakKata() {
 		listKata = new ArrayList<String>();
 		listKata.add("buku");
 		listKata.add("kucing");
 		listKata.add("pusheen");
 	}
-
+	
+	/**
+	 * Method yang digunakan untuk menambahkan kata yang diinginkan
+	 * @param yang diterima berupa String
+	 * @return void
+	 */
 	public void addKata(String kata) {
 		listKata.add(kata);
 	}
 	
+	/**
+	 * Method yang digunakan untuk mengambil kata acak
+	 * @param berupa integer hasil random
+	 * @return String yang diambil dari listKata dengan index
+	 * i berupa nilai hasil randomize
+	 */
 	public String getKata(int i) {
 		return listKata.get(i);
 	}
-
+	
+	/**
+	 * Method yang digunakan untuk melakukan randomisasi index
+	 * index ini nantinya digunakan untuk mendapatkan kata pada
+	 * listKata index ke i
+	 * @return index hasil randomisasi
+	 */
 	public int genRandom() {
 		Random seed = new Random();
 		int i =  seed.nextInt(listKata.size());
 		return i;
 	}
 
+	/**
+	 * Method yang digunakan untuk melakukan proses acak kata
+	 * @param String awal yang akan diacak
+	 * @return String yang sudah diacak
+	 */
 	public String randomKata(String init) {
 		StringBuilder build = new StringBuilder();
 		String tmp = init;
@@ -60,14 +85,21 @@ public class AcakKata {
 		}
 		return tmp;
 	}
-
+	
+	/**
+	 * Method yang digunakan untuk memberikan petunjuk mengenai option 
+	 * apa saja yang disediakan
+	 */
 	public void getOption() {
 		String[] choices = {"Masukkan pilihan", "===================================", "", "Ketik [INPUT] untuk memasukkan kata", "Ketik [MAIN] untuk bermain", "Ketik [EXIT] untuk keluar"};
 		for(int i =0; i<choices.length; i++)
 			System.out.println(choices[i]);
 		System.out.println();
 	}
-
+	
+	/**
+	 * Method yang digunakan untuk melakukan proses permainan
+	 */
 	public void startANewGame() throws IOException{
 		String init = getKata(genRandom());
 		String curr = randomKata(init);
@@ -104,6 +136,9 @@ public class AcakKata {
 		
 	}
 
+	/**
+	 * main method
+	 */
 	public static void main(String args[]) throws IOException {
 		AcakKata hm = new AcakKata();
 		hm.play();
